8bc9e3990e37e66a397d3dc4a7f1964b406099be
NIKHIL GUPTA <nikhilgupta8800@gmail.com>
2022-07-08 00:07:40 +0530

    minor update

--- src/@constant/config.js
+++ src/@constant/config.js
@@ -1,6 +1,6 @@
 const DEV_API_URL = "http://localhost:8080";
 const STAGING_API_URL = "";
-const PROD_API_URL = "https://erp-maths-dtu-api.herokuapp.com/";
+const PROD_API_URL = "https://erp-maths-dtu-api.herokuapp.com";
 const BASE_URL = PROD_API_URL;
 
 export { DEV_API_URL, STAGING_API_URL, PROD_API_URL, BASE_URL };
